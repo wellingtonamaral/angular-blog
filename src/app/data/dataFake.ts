e1fbeaa74b22ccd756e96f7b3f5224afb3b7d9e5
wellingtonamaral <well.ribeiro@live.com>
2023-10-07 12:42:41 -0300

    Finalizando

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,13 +1,30 @@
 export const dataFake = [{
+    /*Expansão*/
     'id':'1',
-    'picture':'https://ghost.nwdb.info/content/images/2023/08/image--53-.jpg',
-    'title':'(1)nova expansão',
-    'description':'(1)disponivel na steam'
+    'picture':'https://images.ctfassets.net/j95d1p8hsuun/2voiDzWBlgCOZ7J0DeRyer/4f415fce75faacffefc82c54be41089f/NW_Beetle_PVE_ROTAE_Announce_1920x1080_Final_CT-002143.jpg',
+    'title':'O verde exuberante das Selvas Elysian',
+    'description':'A Primeira Luz já sucumbiu. Outrora um espaço acolhedor para recém-chegados a Aeternum, seus campos foram devastados pela fúria da Terra Indócil. Não se sabe o que foi feito das pessoas e aldeias que povoaram a região, e uma barreira mortal reserva a tentativa de descobrir apenas aos mais intrépidos. Essa abundância de poderes terrenos despertou os poderosos Lordes Feras, prováveis novos adversários que o povo de Aeternum terá que enfrentar. Mas com eles vem também uma dádiva: a sabedoria para domar e montar animais. A região no extremo sudeste de Aeternum, antes conhecida como Primeira Luz, foi invadida pelas forças de Ártemis e da Terra Indócil. Explore as estruturas elevadas dessa nova e mágica paisagem e batalhe contra bestas ferozes que buscaram abrigo no território inóspito e verdejante. Embarque nesta nova e perigosa expedição de nível 62+ para encontrar os Lordes Feras primordiais antes que o caos irrompa das profundezas de Aeternum.'
 
 },{
+    /**Chamado das Selvas */
     'id':'2',
-    'picture':'https://images.ctfassets.net/j95d1p8hsuun/6dPkjTa9NiZ2nD41CiZhBd/c70affc7f9a849983c4e035644642dd7/callofwilds-5802.jpg',
-    'title':'(2)O chamado da Selva',
-    'description':'(2)Novo mapa +65'
+    'picture':'https://images.ctfassets.net/j95d1p8hsuun/3xAuMAueJO3pOYbEoJ992T/113b1743d7cdd5de34745a90efe978ec/NW_CallOfTheWilds_1920x1080.jpg',
+    'title':'O Chamado das Selvas',
+    'description':'Saudações, aventureiros! No dia 3 de outubro, responda ao Chamado das Selvas para comemorar durante uma semana a nossa primeira expansão, Ascensão da Terra Indócil. A cada dia, vamos explorar um novo recurso sob o olhar dos seus streamers favoritos da Twitch. Confira as Corridas de Montarias, descubra os builds do Mangual, domine a Divisão Selvagem, explore as Selvas Elysian e assista a um combate massivo em mundo aberto para obter Twitch Drops de eventos específicos. Como atender a esse chamado?'
+},{
+    /**Chamado das Selvas */
+    'id':'3',
+    'picture':'https://images.ctfassets.net/j95d1p8hsuun/5HwuvX6u1vGwDIwlbzVyfc/7117ec61d2d04709da6c924f3a3a2edc/image__32__1920x1080.jpg',
+    'title':'Montarias: Leões, Cavalos e Lobos',
+    'description':'Convoque uma montaria para viajar por toda Aeternum com mais rapidez. Dome cavalos, lobos atrozes e leões, cada um com visual único, equipamento personalizado e o nome que você escolher. Suba o nível da nova Habilidade Mercante de Montaria para ganhar aprimoramentos, como aumento de velocidade, buffs, acréscimos e alimentos consumíveis de escala superior para seu novo amigo.'
+},{
+    /**Chamado das Selvas */
+    'id':'4',
+    'picture':'https://images.ctfassets.net/j95d1p8hsuun/5owvKczbTYj8dpUFmdTNDl/7b3e51fe5486342ddcbf3afd39968ac8/NW_Flail_HeroShot_ROTAE_Announce_1920x1080_Final_CT-002143.jpg',
+    'title':'Montarias: Leões, Cavalos e Lobos',
+    'description':'O Mangual é uma arma versátil de uma mão que permite a utilização de um escudo na outra mão. Use magia arcana para aprimorar ataques e aplicar buffs em aliados ou debuffs em inimigos e assim facilitar sua vitória. A combinação de combate corpo a corpo, magia arcana e defesas sólidas permite ao portador da arma reforçar a eficácia do grupo sem baixar a ofensiva. ',
+     
 }
+
+
 ]
